4b452d8ef7050b76ae55a7238f9650950a03539c
Eamon <galwaycoder@users.noreply.github.com>
2018-06-12 09:13:33 +0100

    MongoDB added

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
-
 
 app.use(morgan('tiny'));
 // make static files available to express
@@ -17,11 +17,22 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const nav = [
+  { link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' }];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
+
+app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
+
 app.get('/', (req, res) => {
   res.render(
     'index',
     {
-      nav: ['Books', 'Authors'],
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
       title: 'Library'
     }
   );
